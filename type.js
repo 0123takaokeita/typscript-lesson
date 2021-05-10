1d79c6cf8c0eb5bd2a744ed19f4c578d05de50af
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-10 19:07:39 +0900

    配列型とオブジェクト型

--- type.js
+++ type.js
@@ -1,49 +1,32 @@
 // // 真偽値の型は他と同じ考えでもいい。
 // let hasValue: boolean = true;
-
 // // number型は整数、浮動小数、符号付きを定義できる。
 // let count: number = 10;
 // let float: number = 3.14;
 // let minus: number = -0.12;
-
 // // stringは文字列で記号は３パターン使用できる。
 // let single: string = 'hello';
 // let double: string = "hello";
 // let back: string = `hello`;
-
-
 // 型注釈を使用すると冗長になるため基本的には型推論を使用しよう。
 // 真偽値の型は他と同じ考えでもいい。
-let hasValue = true;
-
+var hasValue = true;
 // number型は整数、浮動小数、符号付きを定義できる。
-let count = 10;
-let float = 3.14;
-let minus = -0.12;
-
+var count = 10;
+var float = 3.14;
+var minus = -0.12;
 // stringは文字列で記号は３パターン使用できる。
-let single = 'hello';
-let double = "hello";
-let back = `hello`;
-
-
+var single = 'hello';
+var double = "hello";
+var back = "hello";
 // オブジェクト型を型推論で書いた場合
-const person: {
-  // 型注釈で書いた場合。
-  name: string;
-  age: number;
-} = {
-  name:'Jack',
-  age: 21
-}
+var person = {
+    name: 'Jack',
+    age: 21
+};
 console.log(person.name);
-
 // 配列型
-const fruits: string[] = ['aple','banana','grape']
-const fruit = fruits[0];
+var fruits = ['aple', 'banana', 'grape'];
+var fruit = fruits[0];
 // stringが使用するメソッドが使用できる。
-fruit.length
-
-
-
-
+fruit.length;
