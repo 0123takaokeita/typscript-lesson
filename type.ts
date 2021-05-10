798b362cdbc6ab07264cbca98ecb6135229712f9
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-10 18:32:51 +0900

    型推論で書くことで省略できる

--- type.ts
+++ type.ts
@@ -12,4 +12,20 @@ let double: string = "hello";
 let back: string = `hello`;
 
 
+// 型注釈を使用すると冗長になるため基本的には型推論を使用しよう。
+// 真偽値の型は他と同じ考えでもいい。
+let hasValue = true;
+
+// number型は整数、浮動小数、符号付きを定義できる。
+let count = 10;
+let float = 3.14;
+let minus = -0.12;
+
+// stringは文字列で記号は３パターン使用できる。
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+
+
+
 
